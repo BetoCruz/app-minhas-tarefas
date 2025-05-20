698771b1ceede4b3e5b848b4058dec947b19e9fc
BetoCruz <humbertocruz.sp@gmail.com>
2025-05-20 17:21:51 -0300

    exibindo resultados de filtragem no componente

--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -1,18 +1,55 @@
+import { useDispatch, useSelector } from 'react-redux'
 import FiltroCard from '../../components/FiltroCard'
 import * as S from './styles'
+import { RootReducer } from '../../store'
+import { alterarTermo } from '../../store/reducers/filtro'
+import * as enums from '../../utils/enums/Tarefa'
 
 const BarraLateral = () => {
+  const dispatch = useDispatch()
+  const { termo } = useSelector((state: RootReducer) => state.filtro)
   return (
     <S.Aside>
       <div>
-        <S.Campo type="text" placeholder="Buscar" />
+        <S.Campo
+          type="text"
+          placeholder="Buscar"
+          value={termo}
+          onChange={(evento) => dispatch(alterarTermo(evento.target.value))}
+        />
         <S.Filtros>
-          <FiltroCard legenda="pendentes" contador={1} />
-          <FiltroCard legenda="concluidos" contador={2} />
-          <FiltroCard legenda="urgentes" contador={3} />
-          <FiltroCard legenda="importantes" contador={4} />
-          <FiltroCard legenda="normal" contador={5} />
-          <FiltroCard legenda="todas" contador={10} ativo />
+          <FiltroCard
+            valor={enums.Status.PENDENTE}
+            criterio="status"
+            legenda="pendentes"
+            // contador={1}
+          />
+          <FiltroCard
+            valor={enums.Status.CONCLUIDA}
+            criterio="status"
+            legenda="concluidos"
+            // contador={2}
+          />
+          <FiltroCard
+            valor={enums.Prioridade.URGENTE}
+            criterio="prioridade"
+            legenda="urgentes"
+            // contador={3}
+          />
+          <FiltroCard
+            valor={enums.Prioridade.IMPORTANTE}
+            criterio="prioridade"
+            legenda="importantes"
+            // contador={4}
+          />
+          <FiltroCard
+            valor={enums.Prioridade.NORMAL}
+            criterio="prioridade"
+            legenda="normal"
+            // contador={5}
+          />
+          {/* <FiltroCard criterio="todas" legenda="todas" contador={10} ativo /> */}
+          <FiltroCard criterio="todas" legenda="todas" />
         </S.Filtros>
       </div>
     </S.Aside>
--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -35,12 +35,61 @@ import { RootReducer } from '../../store'
 
 const ListaDeTarefas = () => {
   const { itens } = useSelector((state: RootReducer) => state.tarefas)
+  const { termo, criterio, valor } = useSelector(
+    (state: RootReducer) => state.filtro
+  )
+
+  const filtraTarefas = () => {
+    let tarefasFiltradas = itens
+    if (termo !== undefined) {
+      tarefasFiltradas = tarefasFiltradas.filter(
+        (item) => item.titulo.toLowerCase().search(termo) >= 0
+      )
+
+      if (criterio === 'prioridade') {
+        tarefasFiltradas = tarefasFiltradas.filter(
+          (item) => item.prioridade === valor
+        )
+      } else if (criterio === 'status') {
+        tarefasFiltradas = tarefasFiltradas.filter(
+          (item) => item.status === valor
+        )
+      }
+
+      return tarefasFiltradas
+    } else {
+      return itens
+    }
+  }
+
+  const exibeResultadoFiltragem = (quantidade: number) => {
+    let mensagem = ''
+    if (criterio === 'todas') {
+      mensagem = `${quantidade} tarefas encontradas como: todas ${
+        termo !== undefined && termo.length > 0 ? `e "${termo}"` : ''
+      }`
+    } else {
+      mensagem = `${quantidade} tarefas encontradas como: "${`${criterio}=${valor}`} ${
+        termo !== undefined && termo.length > 0 ? `e "${termo}"` : ''
+      }"`
+    }
+  }
+
+  const tarefas = filtraTarefas()
 
   return (
     <Container>
-      <p>2 tarefas marcadas como : &quot;categoria&quot; e &quot;termo&quot;</p>
+      <p>
+        {tarefas.length} tarefas marcadas como:&quot;{`${criterio}=${valor}`}
+        &quot; {termo !== undefined && termo.length > 0 ? `e "${termo}"` : ``}
+      </p>
       <ul>
-        {itens.map((t) => (
+        <li>{termo}</li>
+        <li>{criterio}</li>
+        <li>{valor}</li>
+      </ul>
+      <ul>
+        {tarefas.map((t) => (
           <li key={t.titulo}>
             <Tarefa
               id={t.id}
